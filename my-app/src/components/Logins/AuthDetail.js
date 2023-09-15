d0452aa011939ac91ae991a4f9e6f334db10b2b6
Anisha Kumari Lal <96767716+Fr0styfeet@users.noreply.github.com>
2023-09-15 20:58:03 +0530

    after profile routing

--- my-app/src/components/Logins/AuthDetail.js
+++ my-app/src/components/Logins/AuthDetail.js
@@ -41,9 +41,54 @@ export default function AuthDetails() {
 
   return (
     <div className='d-flex justify-content-end'>
-      {authUser?<><p>{`${authUser.email} |`} 
+      {authUser?<><p><p onClick={handleprofile}>{`${authUser.email} |`}</p> 
       <button className="btn mb-1" onClick={userSignOut}>Logout</button></p>
       </>:<p>Guest |<button className="btn mb-1"><a href='/login'>Login</a></button></p>}
     </div>
   )
 }
+
+// export default function AuthDetails() {
+//     const[authUser,setAuthUser]=useState(null);
+
+//     useEffect(()=>{
+//         const listen =onAuthStateChanged(auth,(user)=>{
+//             if(user){
+//                 setAuthUser(user);
+//             }
+//             else{
+//                 setAuthUser(null);
+//             }
+//         })
+//         return ()=>{
+//             listen();
+//         }
+//     },[]);
+
+//     const userSignOut=()=>{
+//         signOut(auth).then(()=>{
+//             console.log("signed out")
+//         }).catch(error=>{
+//             console.log(error);
+//         })
+//     }
+//   let navigate=useNavigate();
+
+
+//     const handleprofile=()=>{
+//         navigate({
+//             pathname:"/userprofile",
+            
+//         }
+//         )
+//     }
+
+//   return (
+//     <div className='d-flex justify-content-end'>
+//       {authUser?<><p>{`${authUser.email} |`} 
+//       <button className="btn mb-1" onClick={userSignOut}>Logout</button></p>
+//       </>:<p>Guest |<button className="btn mb-1"><a href='/login'>Login</a></button></p>}
+//     </div>
+//   )
+// }
+
